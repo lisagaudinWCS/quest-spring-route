3832f75bc7be1a51e98bac3d537655261c864e61
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-11-15 10:43:33 +0100

    finito

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,31 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        if (number > 0 && number < 13) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Toto");
+        } else if (number == 13) {
+            return new Doctor(13, "Jodie Wittaker");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
+        // return null;
     }
 }
+
+// if {number} == 13 tu me renvoies le JSON {"number":13, "name":"Toto"}
+// if {number} > 0 && < 13 tu me renvoies un statut : 303 See Other
+//if {number} n'existe pas tu me renvoies un statut : 404 Not Found
